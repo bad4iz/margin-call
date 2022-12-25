e4b28cb69c5f1600e3ccbf2c15d1249a58df9a34
bad4iz <developer@bad4iz.ru>
2022-12-25 16:04:45 +0400

    webrtc init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
 import './index.css'
 import { setupCounter } from './counter.js';
+import Peer from 'simple-peer/simplepeer.min.js'
+
+const connect = document.getElementById('connect');
+const yourID = document.getElementById('yourID');
+const otherID = document.getElementById('otherID');
+const yourMessage = document.getElementById('yourMessage');
+const messages = document.getElementById('messages');
+const send = document.getElementById('send');
+
+
+const peer = new Peer({initiator: location.hash === '#init', trickle: false})
+// const peer2 = new Peer()
+peer.on('error', err => console.log('error', err))
+peer.on('signal', function(data){
+
+  yourID.value = JSON.stringify(data)
+})
+
+connect.addEventListener('click', ()=> {
+  peer.signal(otherID.value)
+})
+
+send.addEventListener('click', ()=> {
+  peer.send(yourMessage.value)
+})
+
+peer.on('data', (data)=>{
+  messages.textContent += data + '\n'
+})
+
 
 setupCounter(document.querySelector('#counter'));
