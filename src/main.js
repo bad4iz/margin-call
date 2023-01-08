f0870994b5d28c54289f641a767d95fc71620253
bad4iz <developer@bad4iz.ru>
2023-01-08 16:07:21 +0400

    ice server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './index.css'
+import './index.css';
 import { setupCounter } from './counter.js';
-import Peer from 'simple-peer/simplepeer.min.js'
+import Peer from 'simple-peer/simplepeer.min.js';
+import freeice from 'freeice';
 
 const connect = document.getElementById('connect');
 const yourID = document.getElementById('yourID');
@@ -9,26 +10,27 @@ const yourMessage = document.getElementById('yourMessage');
 const messages = document.getElementById('messages');
 const send = document.getElementById('send');
 
-
-const peer = new Peer({initiator: location.hash === '#init', trickle: false})
+const peer = new Peer({
+  initiator: location.hash === '#init',
+  trickle: false,
+  config: { iceServers: freeice() },
+});
 // const peer2 = new Peer()
-peer.on('error', err => console.log('error', err))
-peer.on('signal', function(data){
+peer.on('error', (err) => console.log('error', err));
+peer.on('signal', function (data) {
+  yourID.value = JSON.stringify(data);
+});
 
-  yourID.value = JSON.stringify(data)
-})
+connect.addEventListener('click', () => {
+  peer.signal(otherID.value);
+});
 
-connect.addEventListener('click', ()=> {
-  peer.signal(otherID.value)
-})
+send.addEventListener('click', () => {
+  peer.send(yourMessage.value);
+});
 
-send.addEventListener('click', ()=> {
-  peer.send(yourMessage.value)
-})
-
-peer.on('data', (data)=>{
-  messages.textContent += data + '\n'
-})
-
+peer.on('data', (data) => {
+  messages.textContent += data + '\n';
+});
 
 setupCounter(document.querySelector('#counter'));
